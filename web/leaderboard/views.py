a9936f5729a72761dd464f9db8687144d7fc8bb5
ThiagoCabral501 <thiago.cabral@davinci.edu.ar>
2026-07-08 21:48:16 -0300

    Filtros de ordenamietno del leaderboard
    Implementacion de pestañas para ordenar por puntaje (mayor/menor) y fecha (reciente/antiguo)
    Integracion de persistencia de estados entre el buscador de jugadores y el orden seleccionado
    Ajuste condicional en las medallas del podio para que solo muestren esteticamente en el ordenamiento por mayor puntaje

--- web/leaderboard/views.py
+++ web/leaderboard/views.py
@@ -4,21 +4,31 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Partida
 
-
 def home(request):
     return render(request, 'leaderboard/home.html')
-
 
 def leaderboard(request):
     partidas = Partida.objects.all()
     player = request.GET.get('player', '')
+    orden = request.GET.get('orden', 'puntaje_desc')
+
     if player:
         partidas = partidas.filter(jugador__icontains=player)
+
+    if orden == 'puntaje_desc':
+        partidas = partidas.order_by('-puntaje', '-fecha')
+    elif orden == 'puntaje_asc':
+        partidas = partidas.order_by('puntaje', '-fecha')
+    elif orden == 'reciente':
+        partidas = partidas.order_by('-fecha')
+    elif orden == 'antiguo':
+        partidas = partidas.order_by('fecha')
+
     return render(request, 'leaderboard/leaderboard.html', {
         'scores': partidas,
         'current_player': player,
+        'current_orden': orden,
     })
-
 
 @csrf_exempt
 def api_add_score(request):
